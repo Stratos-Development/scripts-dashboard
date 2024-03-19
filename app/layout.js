8d27bfd7c373181077603180fc02b293619e76fe
Tundra <78630930+Tundra8946@users.noreply.github.com>
2024-03-18 21:50:17 -0400

    trying

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 "use client";
 import './css/style.css'
 import { Inter, Architects_Daughter } from 'next/font/google'
+import Header from '@/components/ui/header'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -23,6 +24,7 @@ export default function RootLayout({
       <title>Stratos Tech</title>
       <body className={`${inter.variable} ${architects_daughter.variable} [&::-webkit-scrollbar]:[width:12px] [&::-webkit-scrollbar-thumb]:rounded-xl [&::-webkit-scrollbar-thumb]:bg-purple-600 [&::-webkit-scrollbar-track]:bg-gray-900 [&::-webkit-scrollbar-track]:rounded-xl font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden" >
+          <Header />
           {children}
         </div>
       </body>
