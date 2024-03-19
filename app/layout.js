2af4621784a1fa7bc82c80faecbc8400780d2015
Tundra <78630930+Tundra8946@users.noreply.github.com>
2024-03-18 21:42:49 -0400

    bug: trying something

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,6 @@
 "use client";
 import './css/style.css'
-import React, { useState, useEffect } from 'react';
-
 import { Inter, Architects_Daughter } from 'next/font/google'
-import Header from '@/components/ui/header'
-import Loader from '@/components/loader';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -22,25 +18,12 @@ const architects_daughter = Architects_Daughter({
 export default function RootLayout({
   children
 }) {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => { setLoading(false) }, 300)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  function Loaded() {
-    return (
-      <>
-        <Header />
-        {children}
-      </>
-    )
-  }
   return (
     <html lang="en"  >
       <title>Stratos Tech</title>
       <body className={`${inter.variable} ${architects_daughter.variable} [&::-webkit-scrollbar]:[width:12px] [&::-webkit-scrollbar-thumb]:rounded-xl [&::-webkit-scrollbar-thumb]:bg-purple-600 [&::-webkit-scrollbar-track]:bg-gray-900 [&::-webkit-scrollbar-track]:rounded-xl font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden" >
-          {loading ? <Loader /> : <Loaded />}
+          {children}
         </div>
       </body>
     </html>
