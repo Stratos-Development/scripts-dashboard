a3f124712065b68c6ff522c52d145b5327e1b842
Tundra <78630930+Tundra8946@users.noreply.github.com>
2024-03-18 21:00:37 -0400

    bug: fixing api route list not working

--- app/api/list/route.js
+++ app/api/list/route.js
@@ -1,31 +1,31 @@
-const list = []
 
-export async function GET(req) {
-    await fetch('https://api.github.com/repos/Stratos-Development/Simple-Scripts/contents/Docker%20Setups', {
-        headers: {
-            'Accept': 'application/vnd.github+json',
-            'Authorization': process.env.GITHUB_TOKEN,
-            'X-GitHub-Api-Version': '2022-11-28'
-        }
-    }).then((response) => response.json())
-        .then(async (data) => {
-            await data.map(async (x) => {
-                await fetch(`https://api.github.com/repos/Stratos-Development/Simple-Scripts/readme/Docker%20Setups/${String(x.name).replace(" ", "%20")}`, {
-                    headers: {
-                        'Accept': 'application/vnd.github+json',
-                        'Authorization': process.env.GITHUB_TOKEN,
-                        'X-GitHub-Api-Version': '2022-11-28'
-                    }
-                }).then((response) => response.json())
-                    .then(async (data) => {
-                        if (list.map(a => a.name).includes(x.name)) return;
-                        return list.push({ name: x.name, desc: Buffer.from(data.content, 'base64').toString('ascii') })
-                    })
-            })
+let list = []
+await fetch('https://api.github.com/repos/Stratos-Development/Simple-Scripts/contents/Docker%20Setups', {
+    headers: {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': process.env.GITHUB_TOKEN,
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+}).then((response) => response.json())
+    .then(async (data) => {
+        await data.map(async (x) => {
+            await fetch(`https://api.github.com/repos/Stratos-Development/Simple-Scripts/readme/Docker%20Setups/${String(x.name).replace(" ", "%20")}`, {
+                headers: {
+                    'Accept': 'application/vnd.github+json',
+                    'Authorization': process.env.GITHUB_TOKEN,
+                    'X-GitHub-Api-Version': '2022-11-28'
+                }
+            }).then((response) => response.json())
+                .then(async (data) => {
+                    if (list.map(a => a.name).includes(x.name)) return;
+                    list.push({ name: x.name, desc: Buffer.from(data.content, 'base64').toString('ascii') })
+                })
         })
-        .catch((err) => {
-            console.log(err.message);
-        });
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+export async function GET(req) {
     return Response.json({
         list: JSON.parse(JSON.stringify(list))
     }, {
