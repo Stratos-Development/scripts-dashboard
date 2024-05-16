11f0eb169c789c39ce48ca77b8664a9831e619c3
Tundra <78630930+Tundra8946@users.noreply.github.com>
2024-05-16 13:43:18 -0400

    refactor: Fixing the link to the bash scripts to be correct

--- app/(default)/page.js
+++ app/(default)/page.js
@@ -36,7 +36,7 @@ export default function Hero() {
                       Docker Scripts
                       <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6" /></svg>
                     </a>
-                    <a href="/scripts" className="drop-shadow-xl shadow-black py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-[#3c4549] text-white hover:bg-[#2d3437] disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                    <a href="/sh" className="drop-shadow-xl shadow-black py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-[#3c4549] text-white hover:bg-[#2d3437] disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
                       Bash Scripts
                       <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6" /></svg>
                     </a>
