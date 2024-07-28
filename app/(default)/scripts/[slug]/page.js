fd1a2bf3bd22f7d759b778454ee5a40192a70ba0
Tundra <78630930+Tundra8946@users.noreply.github.com>
2024-07-27 21:57:46 -0400

    feat: Fixing the search bar and adding in loading for scripts

--- app/(default)/scripts/[slug]/page.js
+++ app/(default)/scripts/[slug]/page.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import Markdown from 'react-markdown'
 import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
@@ -7,39 +7,50 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 export default function Hero({ params }) {
     const [data, setData] = useState([]);
     const [markdown1, setMarkdownData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
         fetch(`https://scripts-api.stratostech.xyz/scripts/${params.slug}`).then((response) => response.json())
             .then((data) => {
-                setData(data.dockerCompose)
-                setMarkdownData(data.instructions)
+                setData(data.dockerCompose);
+                setMarkdownData(data.instructions);
+                setIsLoading(false); // Set loading to false after data is fetched
             })
             .catch((err) => {
                 console.log(err.message);
+                setIsLoading(false); // Set loading to false even if there is an error
             });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
     function CC({ content }) {
-        return (<svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 active:translate-y-1 hover:cursor-pointer" fill="none" viewBox="0 0 24 19" stroke="currentColor" strokeWidth={2} onClick={() => navigator.clipboard.writeText(content)}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
-        </svg>)
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 active:translate-y-1 hover:cursor-pointer" fill="none" viewBox="0 0 24 19" stroke="currentColor" strokeWidth={2} onClick={() => navigator.clipboard.writeText(content)}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+            </svg>
+        )
     }
+
     return (
         <section>
             <div className="mx-auto relative">
-                <div className="mx-auto block items-center bg-gray-800 rounded-lg shadow-xl max-w-[100rem] p-10 mt-10 row text-left grid sm:grid-cols-2 lg:grid-cols-2 gap-2">
-                    <Markdown className='px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto'>{markdown1.toString()}</Markdown>
-                    <div className="item- px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+                <div className="mx-auto block items-center bg-gray-800 rounded-lg shadow-xl max-w-[100rem] mb-10 drop-shadow-xl p-10 mt-10 row text-left grid sm:grid-cols-2 lg:grid-cols-2 gap-2 relative">
+                    {isLoading && (
+                        <div className="absolute inset-0 h-[1000px] bg-gray-800 flex items-center justify-center z-10 rounded-lg">
+                            <div className="w-8 h-8 border-4 border-t-4 border-t-transparent border-white rounded-full animate-spin"></div>
+                        </div>
+                    )}
+                    <Markdown className='px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto z-0'>{markdown1.toString()}</Markdown>
+                    <div className="item- px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto z-0">
                         <div className="grid w-[45rem] mx-auto items-center gap-2">
-                            <p className='inline-flex items-baseline gap-1'>Click on the icon to copy the code below  <CC content={data} /></p>
+                            <p className='inline-flex items-baseline gap-1'>Click on the icon to copy the code below <CC content={data} /></p>
                             <SyntaxHighlighter language="yml" style={tomorrow} className="drop-shadow-xl">
                                 {data}
                             </SyntaxHighlighter>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </section>
     )
 }
--- components/pages/scripts.js
+++ components/pages/scripts.js
@@ -31,51 +31,53 @@ export default function Hero() {
     const skeletons = Array(23).fill(0);
 
     return (
-        <section className="bg-gray-900 text-white ">
-            <div className="max-w-[104rem] mx-auto px-6 lg:px-8 py-10">
+        <section className="bg-gray-900 text-white">
+            <div className="w-[104rem] mx-auto px-6 lg:px-8 py-10">
                 <div className="relative">
                     <input
                         type="text"
-                        className="w-full mb-6 p-3 rounded-lg border border-gray-700 bg-gray-800 text-gray-200 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                        className="w-full mb-6 p-3 drop-shadow-xl rounded-lg border border-gray-700 bg-gray-800 text-gray-200 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
                         placeholder="Search scripts..."
                         value={searchTerm}
                         onChange={(e) => setSearchTerm(e.target.value)}
                     />
-                    {loading ? (
-                        <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-3 sm:gap-6">
-                            {skeletons.map((_, index) => (
-                                <div key={index} className="h-[120px] w-[300px] group flex flex-col border shadow-lg rounded-xl transition bg-gray-800 hover:bg-gray-850 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-                                    <div className="p-4 md:p-5 animate-pulse">
-                                        <div className="flex">
-                                            <div className="grow ms-5">
-                                                <div className="h-6 bg-gray-700 rounded mb-2"></div>
-                                                <div className="h-4 bg-gray-700 rounded"></div>
+                    <div className="min-h-[500px]">
+                        {loading ? (
+                            <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-3 sm:gap-6">
+                                {skeletons.map((_, index) => (
+                                    <div key={index} className="h-[120px] w-[300px] group flex flex-col border shadow-lg rounded-xl transition bg-gray-800 hover:bg-gray-850 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                                        <div className="p-4 md:p-5 animate-pulse">
+                                            <div className="flex">
+                                                <div className="grow ms-5">
+                                                    <div className="h-6 bg-gray-700 rounded mb-2"></div>
+                                                    <div className="h-4 bg-gray-700 rounded"></div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            ))}
-                        </div>
-                    ) : (
-                        <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-3 sm:gap-6">
-                            {filteredData.map(x => (
-                                <a key={x.name} className="transform hover:scale-105 transition-transform duration-300 ease-in-out h-[120px] group flex flex-col border shadow-lg rounded-xl transition bg-gray-800 hover:bg-gray-850 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href={"/scripts/" + x.name}>
-                                    <div className="p-4 md:p-5">
-                                        <div className="flex">
-                                            <div className="grow ms-5">
-                                                <h3 className="font-semibold group-hover:text-gray-400 text-gray-200">
-                                                    {x.name}
-                                                </h3>
-                                                <p className="text-sm text-gray-400">
-                                                    {x.desc}
-                                                </p>
+                                ))}
+                            </div>
+                        ) : (
+                            <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-3 sm:gap-6">
+                                {filteredData.map(x => (
+                                    <a key={x.name} className="transform hover:scale-105 transition-transform duration-300 ease-in-out h-[120px] group flex flex-col border drop-shadow-lg rounded-xl transition bg-gray-800 hover:bg-gray-850 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href={"/scripts/" + x.name}>
+                                        <div className="p-4 md:p-5">
+                                            <div className="flex">
+                                                <div className="grow ms-5">
+                                                    <h3 className="font-semibold group-hover:text-gray-400 text-gray-200">
+                                                        {x.name}
+                                                    </h3>
+                                                    <p className="text-sm text-gray-400">
+                                                        {x.desc}
+                                                    </p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </a>
-                            ))}
-                        </div>
-                    )}
+                                    </a>
+                                ))}
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </section>
